bc454fd07d7a092b1cb80510497e05565a3eb149
Munawwar <munawwarfiroz@hotmail.com>
2023-11-02 00:01:58 +0400

    use esbuild rebuild API for more efficient watch mode
    it keeps past processed JS/other files in memory so that rebuilds are faster. Slashed the time from 20+ ms to 10-15 ms

--- build.js
+++ build.js
@@ -1,5 +1,8 @@
-import { build } from 'esbuild';
+import { build, context } from 'esbuild';
 import glob from 'tiny-glob';
+import chokidar from 'chokidar';
+import { parseArgs } from 'node:util';
+import { performance } from 'node:perf_hooks';
 import rimraf from 'rimraf';
 import {
   publicDirectoryRelative,
@@ -9,6 +12,15 @@ import {
   ssrDirectory
 } from './server/paths.js';
 import { promises as fs } from 'node:fs';
+
+const { values: { watch } } = parseArgs({
+  options: {
+    watch: {
+      type: 'boolean',
+      default: false
+    }
+  }
+});
 
 const [entryPoints] = await Promise.all([
   glob('./client/pages/**/*.page.jsx'),
@@ -38,25 +50,59 @@ const commonConfig = {
   jsx: 'automatic'
 };
 
+const clientBuildConfig = {
+  outdir: publicDirectoryRelative,
+  splitting: true,
+  minify: true,
+  entryNames: '[dir]/[name]-[hash]',
+  metafile: true,
+  ...commonConfig
+};
+const serverBuildConfig = {
+  outdir: ssrDirectoryRelative,
+  splitting: false,
+  minify: false,
+  external: ['preact', 'preact-render-to-string'],
+  ...commonConfig
+};
+
 const [result1, result2] = await Promise.all([
-  build({
-    outdir: publicDirectoryRelative,
-    splitting: true,
-    minify: true,
-    ...commonConfig
-  }),
-  build({
-    outdir: ssrDirectoryRelative,
-    splitting: false,
-    minify: false,
-    external: ['preact', 'preact-render-to-string'],
-    ...commonConfig
-  })
+  build(clientBuildConfig),
+  build(serverBuildConfig)
 ]);
 
-if (result1 && result1.metafile) {
-  await Promise.all([
-    fs.writeFile(`${publicDirectory}/metafile.json`, JSON.stringify(result1.metafile, 0, 2)),
-    fs.writeFile(`${ssrDirectory}/metafile.json`, JSON.stringify(result2.metafile, 0, 2))
+async function writeMetafile(result1, result2) {
+  if (result1 && result1.metafile) {
+    await Promise.all([
+      fs.writeFile(`${publicDirectory}/metafile.json`, JSON.stringify(result1.metafile, 0, 2)),
+      fs.writeFile(`${ssrDirectory}/metafile.json`, JSON.stringify(result2.metafile, 0, 2))
+    ]);
+  }
+}
+
+writeMetafile(result1, result2);
+
+if (watch) {
+  const watchGlob = 'client/';
+  // mimicking nodemon logs
+  console.debug(`watching path: ${watchGlob}`);
+  console.debug(`watching extensions: (all)`);
+  const [ctx1, ctx2] = await Promise.all([
+    context(clientBuildConfig),
+    context(serverBuildConfig)
   ]);
+
+  chokidar
+    .watch(watchGlob, {
+      ignoreInitial: true
+    })
+    .on('all', async () => {
+      const startTime = performance.now();
+      const [result1, result2] = await Promise.all([
+        ctx1.rebuild(),
+        ctx2.rebuild()
+      ]);
+      writeMetafile(result1, result2);
+      console.log(`rebuilt in ${(performance.now() - startTime).toFixed(2)} ms`);
+    });
 }
